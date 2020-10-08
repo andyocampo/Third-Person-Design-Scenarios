19dcf098639f9e5a64c2b895e41e1f5eb6ea8391
Andy <andyocampo@users.noreply.github.com>
2020-10-08 00:08:17 -0500

    Adds fade out to text and stop coroutine

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ContextualMessageController : MonoBehaviour
 {
+    [SerializeField]
+    private float fadeOutDuration = 1;
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
 
@@ -20,11 +22,21 @@ public class ContextualMessageController : MonoBehaviour
         canvasGroup.alpha = 1;
         messageText.text = message;
         yield return new WaitForSeconds(duration);
+        //start fading out text
+        float fadeElapsedTime = 0;
+        float fadeStartTime = Time.time;
+        while (fadeElapsedTime < fadeOutDuration)
+        {
+            fadeElapsedTime = Time.time - fadeStartTime;
+            canvasGroup.alpha = 1 - fadeElapsedTime / fadeOutDuration;
+            yield return null;
+        }
         canvasGroup.alpha = 0;
     }
 
     private void OnContextualMessageTriggered(string message, float messageDuration)
     {
+        StopAllCoroutines();
         StartCoroutine(ShowMessage(message, messageDuration));
     }
 
