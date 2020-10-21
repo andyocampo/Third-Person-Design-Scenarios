b529962089843424f3a21787fc9f83377a94ad51
Andy <andyocampo@users.noreply.github.com>
2020-10-21 01:34:10 -0500

    Refactors move input handling to use the new input system

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RigidBodyCharacterController : MonoBehaviour
 {
@@ -51,9 +52,8 @@ public class RigidBodyCharacterController : MonoBehaviour
         }
     }
 
-    private void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        input.x = Input.GetAxisRaw("Horizontal");
-        input.y = Input.GetAxisRaw("Vertical");
+        input = context.ReadValue<Vector2>();
     }
 }
